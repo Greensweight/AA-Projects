ab25170c48f0a0a3d98a1e7042f3853988d3d786
andrew <greensweight96@gmail.com>
2021-12-15 23:52:39 -0800

    phase 0 of project

--- Data Structures/PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ Data Structures/PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,13 @@
 class PolyTreeNode
+    ##Phase 1
 
+    attr_accessor :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    
 end
