350d4e6d96b1344078253ae788864cb77785103e
andrew <greensweight96@gmail.com>
2021-12-19 12:06:44 -0800

    Phase 2 complete

--- Data Structures/PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ Data Structures/PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -9,5 +9,36 @@ class PolyTreeNode
         @children = []
     end
 
+    def parent=(parent_node)
+        @parent.children.delete(self) unless @parent == nil
+        @parent = parent_node
+        @parent.children << self unless parent_node == nil
+
+    end
+
+    def add_child(child_node)
+
+    end
+
+    def remove_child(child_node)
     
-end
+    end
+end 
+
+n1 = PolyTreeNode.new("root1")
+n2 = PolyTreeNode.new("root2")
+n3 = PolyTreeNode.new("root3")
+
+# connect n3 to n1
+n3.parent = n1
+
+# connect n3 to n2
+n2.parent = n3
+
+
+# # this should work
+#raise "Bad parent=!" unless n3.parent == n2
+#raise "Bad parent=!" unless n2.children == [n3]
+
+# # this probably doesn't
+#raise "Bad parent=!" unless n1.children == []
