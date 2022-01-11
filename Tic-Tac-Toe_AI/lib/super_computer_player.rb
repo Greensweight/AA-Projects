b65742f2290d6faaec2ecd386d5ee3570d1334aa
andrew <greensweight96@gmail.com>
2022-01-10 22:59:46 -0800

    Tictactoe AI pHase II almost complete, need one more test

--- Tic-Tac-Toe_AI/lib/super_computer_player.rb
+++ Tic-Tac-Toe_AI/lib/super_computer_player.rb
@@ -2,6 +2,15 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark) #forgot the .board
+
+    node = node.children.find { |child| child.winning_node?(mark)} # did not know about 'find' method, this is cool
+    return node.prev_move_pos if node
+    
+    node = node.children.find { |child| !child.losing_node?(mark)}
+    return node.prev_move_pos if node
+
+    raise " there are no winning nodes, srry m8"
   end
 end
 
