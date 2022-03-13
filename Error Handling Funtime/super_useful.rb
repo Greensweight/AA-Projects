2eaf09d571dacb4efc2531779b169937cfd79507
andrew <greensweight96@gmail.com>
2022-03-13 12:08:11 -0700

    Finished the error handling funtime project

--- Error Handling Funtime/super_useful.rb
+++ Error Handling Funtime/super_useful.rb
@@ -1,30 +1,54 @@
-# PHASE 2
-def convert_to_int(str)
-  Integer(str)
-end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
+class CoffeeError < StandardError
+  def message
+    "No more caffeine pls. You can try again."
+  end
+end
+
+class NotAFruitError < StandardError
+  def message
+    "that is not a fruit, go away."
+  end
+end
+
+
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
+  else
+    raise NotAFruitError
+  end
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    puts e.message
+    retry
+  rescue NotAFruitError => e
+    puts e.message
+  end
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    if yrs_known < 5
+      raise ArgumentError.new("You are not best friends lol")
+    end
+    if name.length <= 0 || fav_pastime.length <= 0
+      raise ArgumentError.new("Really... c'mon... these cannot be blank")
+    end
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
--- Error Handling Funtime/user_script.rb
+++ Error Handling Funtime/user_script.rb
@@ -1,10 +1,9 @@
 require_relative 'super_useful'
 
-puts "'five' == #{convert_to_int('five')}"
 
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('a', 5, 'b')
 
 sam.talk_about_friendship
 sam.do_friendstuff
